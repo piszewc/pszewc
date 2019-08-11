e0dac9a128d5846a8da2d9582b7265dced645826
Piotr Szewc <39332276+piszewc@users.noreply.github.com>
2019-08-11 21:01:06 +0200

    ML Model

--- blog/static/js/main.js
+++ blog/static/js/main.js
@@ -12,7 +12,20 @@ $(document).ready(function () {
         var i = p_class+","+sex+","+age+","+fare_band+","+embarked+","+title+","+is_alone+","+age_class
 
         url = "http://127.0.0.1:8000/api/predict/?review="+i;
-        window.open(url);
+        window.open(url)
         
+        fetch(url)
+            .then(function(response) {
+                if(response.ok) { // Check if response went through
+                    response.json().then(function(data) { 
+                        var price_USD = document.getElementsByTagName("pre");
+                        var USDPrice = '<p>USDEUR Price:' + data.something + '</p>';
+                        price_USD.innerHTML += USDPrice;
+                    });
+                } else { // Response wasn't ok. Check dev tools
+                    console.log("response failed?");
+                }
+            });
+
     });
 });
