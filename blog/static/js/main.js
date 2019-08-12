5b22811e2a11c0a42616af4e5ba7186ef1696b64
Piotr Szewc <39332276+piszewc@users.noreply.github.com>
2019-08-12 21:02:19 +0200

    Update main.js

--- blog/static/js/main.js
+++ blog/static/js/main.js
@@ -13,19 +13,19 @@ $(document).ready(function () {
 
         url = "http://127.0.0.1:8000/api/predict/?review="+i;
         window.open(url)
-        
-        fetch(url)
-            .then(function(response) {
-                if(response.ok) { // Check if response went through
-                    response.json().then(function(data) { 
-                        var price_USD = document.getElementsByTagName("pre");
-                        var USDPrice = '<p>USDEUR Price:' + data.something + '</p>';
-                        price_USD.innerHTML += USDPrice;
-                    });
-                } else { // Response wasn't ok. Check dev tools
-                    console.log("response failed?");
-                }
-            });
+        //
+        //fetch(url)
+        //    .then(function(response) {
+        //        if(response.ok) { // Check if response went through
+        //            response.json().then(function(data) { 
+        //               var price_USD = document.getElementsByTagName("pre");
+        //                var USDPrice = '<p>USDEUR Price:' + data.something + '</p>';
+        //                price_USD.innerHTML += USDPrice;
+        //            });
+        //        } else { // Response wasn't ok. Check dev tools
+        //            console.log("response failed?");
+        //        }
+        //    });
 
     });
 });
